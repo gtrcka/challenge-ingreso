4e8816e7b43079b46c85ea9187141185b67e1ab6
Gabriel <trckagabriel@gmail.com>
2022-06-08 19:07:10 -0300

    Initial commit

---
+++ src/main/java/com/alkemy/challengedisney/challengedisney/ChallengedisneyApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.challengedisney.challengedisney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengedisneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengedisneyApplication.class, args);
+	}
+
+}
